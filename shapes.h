481507c7708451b9a2d93ec9199a75ba448556fb
mcoleman2760 <104951107+mcoleman2760@users.noreply.github.com>
2022-09-23 02:42:05 -0400

    Create shapes.h

---
+++ shapes.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <iostream>
+#include <string>
+
+std::string box(int width, int height);
+std::string checkerboard(int width, int height);
+std::string cross(int size);
+std::string lower (int sidelength);
+std::string upper (int sidelengthu);
+std::string trap (int width, int height);
+std::string checkerboard3(int width, int height);
