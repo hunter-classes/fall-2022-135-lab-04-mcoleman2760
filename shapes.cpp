5b27bc5f73dff28dfc9b564ddc2f11b83d78095a
mcoleman2760 <104951107+mcoleman2760@users.noreply.github.com>
2022-09-23 03:26:40 -0400

    Update shapes.cpp

--- shapes.cpp
+++ shapes.cpp
@@ -3,132 +3,143 @@
 
 std::string box(int width, int height) {
   std::string s = "";
- 
-for (int i = 0; i < height;i++){
-  return  "\n";
-   for (int i = 0; i < width;i++){
-     s+= "*";
+    
+    for(int i=0; i<height; i++)
+    {
+        for(int j=0; j<width; j++)
+        {
+            s += "*";
+        }
+        s += "\n";
+    }
+    return s;
 }
-  s += "\n";
-  }
-  return s;
-  }
 
 std::string checkerboard(int width, int height) {
-  std::cout << "Shape: \n" ;
+  
+  std::string s2 = "";
 for (int row = 0; row < height;row++){
 
   for (int col = 0; col < width ;col++){
      if (col %2 == 0 && row % 2 == 0){
-       std::cout << "*";
+        s2 += "*";
     }
     else if (col % 2 != 0 && row % 2 != 0){
-      std::cout << "*";
+       s2 += "*";
       }
     else {
-      std::cout << " ";
+       s2 += " ";
     }
   }
   
       
   
-      std::cout << "\n";
+       s2 +=  "\n";
      }
 
-  return 0;
+  return s2;
   }
 
 std::string cross(int size) {
+      std::string s3 = "";
   for (int rows =1; rows<= size;rows++){
    
    for (int col = 1; col <= size; col++ ) {
    if ((col == rows)  || (col == (size + 1) - rows)){
-     std::cout << "*";
+    s3+= "*";
      }
    
      else {
-       std::cout << " ";
+       s3+= " ";
        }
      
    }
    
-std::cout << std::endl;
+  s3+="\n";
   
  }
-   return 0;
+   return s3;
   }
+
 
 
 std::string lower (int sidelength){
+  std::string s3 = "";
    for (int i =1; i <= sidelength ;i++)
     {
       for (int k = 1; k <= i; k++ ){
-        std::cout << "*";
+        s3 +="*";
     }
-       std::cout << std::endl;
+       s3 +="\n";
   
       }
+  return s3;
 }
 std::string upper (int sidelengthu){
+  std::string s = "";
   for (int i =sidelengthu; i >= 1 ;i--)
     {
       for (int k = sidelengthu - i; k > 0; k-- )
-        std::cout << " ";
+        s+=  " ";
     
       for (int j = i;j > 0; j--){
-        std::cout << "*";
+       s+= "*";
       }
-       std::cout << std::endl;
+       s+="\n";
   
       }
+  return s;
   }
 
 std::string trap (int width, int height){
+   std::string s = "";
   for (int row = 0; row < height; ++row) {
         for (int col = height + row; col > 0; col--) {
 
             if (height % 6 == 1) { 
-        std::cout << "Impossible shape!" << std::endl;
+        return "Impossible shape!\n";
 
-                return 0;
+               
             }
           else {
-            std::cout << " "; 
+            s+= " "; 
             
             }
             }
 
    for (int col = 0; col < (width - (2 * row)); col++) {
-            std::cout << "*";
+           s+="*";
         }
-        std::cout << std::endl; }}
+        s+="\n"; }
+  return s;
+  }
 
 std::string checkerboard3(int width, int height){
-  
+   std::string s = "";
 
 
-char rowStart = '*';
-char c = '*';
+std::string rowStart = "*";
+std::string c = "*";
 for(int row = 1; row <= height; row++){
 c = rowStart; 
 for(int col = 1; col <= width; col++){
 std::cout << c;
 
 if(col % 3 == 0){
-if(c == '*')
+if(c == "*")
 c = ' ';
 else
 c = '*';
 }
 }
-std::cout << std::endl;
+s += "\n";;
 
 if(row % 3 == 0){
-if(rowStart == '*')
-rowStart = ' ';
+if(rowStart == "*")
+s += ' ';
 else
-rowStart = '*';
+s  += '*';
+}  
 }
-}
+return s;
   }
-
